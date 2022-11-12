1ed1674dfb95cdd38fd36de36aee9c5720b1b959
AurelienLouvel <79204651+AurelienLouvel@users.noreply.github.com>
2022-11-12 23:17:16 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,47 +1,57 @@
-import {Scene, PerspectiveCamera, WebGLRenderer, PlaneGeometry, ShaderMaterial, Mesh} from "three";
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  PlaneGeometry,
+  ShaderMaterial,
+  Mesh,
+} from "three";
 import FragmentShader from "./shaders/fragment.glsl";
 import VertexShader from "./shaders/vertex.glsl";
 
 function init() {
+  let WIDTH = window.innerWidth;
+  let HEIGHT = window.innerHeight;
 
-    let WIDTH = window.innerWidth;
-    let HEIGHT = window.innerHeight;
+  const canvas = document.querySelector("canvas");
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
 
-    const canvas = document.querySelector("canvas");
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
+  const renderer = new WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+  });
 
-    const scene = new Scene();
-    const camera = new PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
-    const renderer = new WebGLRenderer({canvas});
+  renderer.setClearColor(0xf6f6f6, 1);
 
-    const geometry = new PlaneGeometry(1, 1, 1, 1);
-    const material = new ShaderMaterial({
-        vertexShader: VertexShader,
-        fragmentShader: FragmentShader
-    });
+  const geometry = new PlaneGeometry(1, 1, 1, 1);
+  const material = new ShaderMaterial({
+    vertexShader: VertexShader,
+    fragmentShader: FragmentShader,
+  });
 
-    const plane = new Mesh(geometry, material);
-    scene.add(plane);
+  const plane = new Mesh(geometry, material);
+  scene.add(plane);
 
-    camera.position.z = 1;
+  camera.position.z = 1;
 
-    function animate() {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-    }
-
-    function onWindowResize() {
-        WIDTH = window.innerWidth;
-        HEIGHT = window.innerHeight;
-        camera.aspect = WIDTH / HEIGHT;
-        camera.updateProjectionMatrix();
-        renderer.setSize(WIDTH, HEIGHT);
-    }
-
+  function animate() {
     requestAnimationFrame(animate);
-    window.addEventListener("resize", onWindowResize, false);
+    renderer.render(scene, camera);
+  }
+
+  function onWindowResize() {
+    WIDTH = window.innerWidth;
+    HEIGHT = window.innerHeight;
+    camera.aspect = WIDTH / HEIGHT;
+    camera.updateProjectionMatrix();
+    renderer.setSize(WIDTH, HEIGHT);
+  }
+
+  requestAnimationFrame(animate);
+  window.addEventListener("resize", onWindowResize, false);
 }
 
 document.addEventListener("DOMContentLoaded", init);
-
